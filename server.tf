c9ef0747914ccbab965ba4da9a98bcbc1ce40181
haribex180 <93118509+haribex180@users.noreply.github.com>
2022-09-16 16:31:53 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -4,12 +4,12 @@ region = "us-east-2"
 resource "aws_instance" "myawsserver" {
   ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.micro"
-  key_name = "gagan-import"
+  key_name = "hari-keyp"
 
   tags = {
-    Name = "Gagan-DevOps-server-IBM"
+    Name = "Hari-IBM"
     env = "production"
-    owner = "Gagandeep"
+    owner = "Haribabu"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /tmp/inv"
